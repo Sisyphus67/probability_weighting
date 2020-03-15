825df6ab8935e2358f14855fc074d69094fa0f5a
Yonatan Berman <bermanjoe@gmail.com>
2020-03-15 19:05:41 -0400

    Added errorbars (as envelopes) in fit

--- figs/python/TK_TF_fit.py
+++ figs/python/TK_TF_fit.py
@@ -5,23 +5,26 @@ import scipy.stats as stat
 
 start = time.time()
 
+# number of standard errors for envelopes
+nse = 1
+
 # Lattimore et al. parameters
-deltaTK = 0.66993
-gammaTK = 0.59479
-deltaTF = 0.77437
-gammaTF = 0.68924
+deltaTK = 0.66993; SEdTK = 0.07
+gammaTK = 0.59479; SEgTK = 0.06
+deltaTF = 0.77437; SEdTF = 0.02
+gammaTF = 0.68924; SEgTF = 0.02
 
 # Gaussian parameters
-locTK = 0.39888
-scaTK = 1.4913
-locTF = 0.23012
-scaTF = 1.3755
+locTK = 0.39888; SElTK = 0.13
+scaTK = 1.4913; SEsTK = 0.2
+locTF = 0.23012; SElTF = 0.03
+scaTF = 1.3755; SEsTF = 0.06
 
 # Student's-t parameters
-nuTK = 0.39034
-deltatTK = 0.28711
-nuTF = 0.46699
-deltatTF = 0.17304
+nuTK = 0.39034; SEnTK = 0.06
+deltatTK = 0.28711; SEdtTK = 0.08
+nuTF = 0.46699; SEnTF = 0.05
+deltatTF = 0.17304; SEdtTF = 0.03
 
 # load data
 TF1995 = np.genfromtxt('TF1995.csv', delimiter=',')
@@ -33,14 +36,25 @@ p = stat.norm.cdf(x, 0, 1)
 pT = stat.nct.cdf(x, 1, 0)
 
 #plot
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
-fig.tight_layout(pad=3.5)
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 3.5))
+fig.tight_layout(pad=3)
 
 #TK1992
 wp1 = (deltaTK * (xx ** gammaTK)) / (deltaTK * (xx ** gammaTK) + ((1 - xx) ** gammaTK))
+dtmpL = deltaTK + nse * SEdTK; gtmpL = gammaTK - nse * SEgTK
+dtmpH = deltaTK - nse * SEdTK; gtmpH = gammaTK + nse * SEgTK
+wp1L = (dtmpL * (xx ** gtmpL)) / (dtmpL * (xx ** gtmpL) + ((1 - xx) ** gtmpL))
+wp1H = (dtmpH * (xx ** gtmpH)) / (dtmpH * (xx ** gtmpH) + ((1 - xx) ** gtmpH))
 wp2 = stat.norm.cdf(x, locTK, scaTK)
+wp2L = stat.norm.cdf(x, locTK - nse * SElTK, scaTK + nse * SEsTK)
+wp2H = stat.norm.cdf(x, locTK + nse * SElTK, scaTK - nse * SEsTK)
 wp3 = stat.nct.cdf(x, df = nuTK, nc = deltatTK, loc = 0, scale = 1)
+wp3L = stat.nct.cdf(x, df = nuTK - nse * SEnTK, nc = deltatTK - nse * SEdtTK, loc = 0, scale = 1)
+wp3H = stat.nct.cdf(x, df = nuTK + nse * SEnTK, nc = deltatTK + nse * SEdtTK, loc = 0, scale = 1)
 
+axes[0].fill_between(xx, wp1L, wp1H, facecolor='b', alpha=0.25, interpolate=True)
+axes[0].fill_between(p, wp2L, wp2H, facecolor='r', alpha=0.25, interpolate=True)
+axes[0].fill_between(pT, wp3L, wp3H, facecolor='0.5', alpha=0.25, interpolate=True)
 axes[0].plot(xx, xx, '--k', label=r'')
 axes[0].axvline(x=0.5, LineStyle='--')
 axes[0].plot(xx, wp1, 'b', lineWidth='3', label=r'$w(p)=\delta p^{\gamma} / (\delta p^{\gamma} + (1-p)^{\gamma})$')
@@ -56,9 +70,20 @@ axes[0].legend(loc='upper left', fontsize='x-small')
 
 #TF1995
 wp1 = (deltaTF * (xx ** gammaTF)) / (deltaTF * (xx ** gammaTF) + ((1 - xx) ** gammaTF))
+dtmpL = deltaTF + nse * SEdTF; gtmpL = gammaTF - nse * SEgTF
+dtmpH = deltaTF - nse * SEdTF; gtmpH = gammaTF + nse * SEgTF
+wp1L = (dtmpL * (xx ** gtmpL)) / (dtmpL * (xx ** gtmpL) + ((1 - xx) ** gtmpL))
+wp1H = (dtmpH * (xx ** gtmpH)) / (dtmpH * (xx ** gtmpH) + ((1 - xx) ** gtmpH))
 wp2 = stat.norm.cdf(x, locTF, scaTF)
+wp2L = stat.norm.cdf(x, locTF - nse * SElTF, scaTF + nse * SEsTF)
+wp2H = stat.norm.cdf(x, locTF + nse * SElTF, scaTF - nse * SEsTF)
 wp3 = stat.nct.cdf(x, df = nuTF, nc = deltatTF, loc = 0, scale = 1)
+wp3L = stat.nct.cdf(x, df = nuTF - nse * SEnTF, nc = deltatTF - nse * SEdtTF, loc = 0, scale = 1)
+wp3H = stat.nct.cdf(x, df = nuTF + nse * SEnTF, nc = deltatTF + nse * SEdtTF, loc = 0, scale = 1)
 
+axes[1].fill_between(xx, wp1L, wp1H, facecolor='b', alpha=0.25, interpolate=True)
+axes[1].fill_between(p, wp2L, wp2H, facecolor='r', alpha=0.25, interpolate=True)
+axes[1].fill_between(pT, wp3L, wp3H, facecolor='0.5', alpha=0.25, interpolate=True)
 axes[1].plot(xx, xx, '--k', label=r'')
 axes[1].axvline(x=0.5, LineStyle='--')
 axes[1].plot(xx, wp1, 'b', lineWidth='3', label=r'$w(p)=\delta p^{\gamma} / (\delta p^{\gamma} + (1-p)^{\gamma})$')
