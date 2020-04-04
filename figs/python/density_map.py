38af02d440b858f2d40172c14a4ba6f0cfc10df4
Ole Peters <ole-peters>
2020-04-04 11:45:41 +0100

    new density figure
    similar to CDF comparison, for symmetry

--- figs/python/density_map.py
+++ figs/python/density_map.py
@@ -10,29 +10,31 @@ xx = np.linspace(0.0, 1.0, num=1000)
 x = np.linspace(-10, 10, num=5000)
 # Gaussians location and scale parameters
 s1 = 1
+#s2 = np.sqrt(3)
 s2 = 2
 l1 = 0
 l2 = 0
 # CDFs
-DO = stat.norm.cdf(x, l1, s1)
-DM = stat.norm.cdf(x, l2, s2)
+DO = stat.norm.pdf(x, l1, s1)
+DM = stat.norm.pdf(x, l2, s2)
 
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 3.5))
 fig.tight_layout(pad=3)
 
 #CDF plot
 #axes[0].axvline(x=-1, LineStyle='--')
-axes[0].axvline(x=0, LineStyle='--')
+#axes[0].axvline(x=0, LineStyle='--')
 #axes[0].axvline(x=1, LineStyle='--')
 #axes[0].axhline(y=0.5, LineStyle='--')
 axes[0].set_xlim((-4.5, 4.5))
-axes[0].plot(x, DO, 'r', lineWidth='2', label=r'$F_{p}(x)$')
-axes[0].plot(x, DM, 'b', lineWidth='2', label=r'$F_{w}(x)$')
+#axes[0].set_ylim((0,.5))
+axes[0].plot(x, DO, 'r', lineWidth='2', label=r'$p(x)$')
+axes[0].plot(x, DM, 'b', lineWidth='2', label=r'$w(x)$')
 
 #plot arrows
-xs=[-2,-1.5,-1,-.5,.5,1,1.5,2]
+xs=[-2.5,-2,-1.5,-1,-.5,0]
 for i in xs:
-    axes[0].arrow(i,stat.norm.cdf(i, l1, s1), 0, stat.norm.cdf(i, l2, s2) - stat.norm.cdf(i, l1, s1), head_width=0.15, head_length=0.045, length_includes_head=True, fc='k', ec='k', zorder=10)
+    axes[0].arrow(i,stat.norm.pdf(i, l1, s1), 0, stat.norm.pdf(i, l2, s2) - stat.norm.pdf(i, l1, s1), head_width=0.16, head_length=0.02, length_includes_head=True, fc='k', ec='k', zorder=10)
 
 #nx = len(x)
 #steps = 2
@@ -44,13 +46,15 @@ for i in xs:
 axes[0].set_xlabel(r'x')
 axes[0].set_ylabel(r'CDFs')
 axes[0].set_xticks(np.arange(-4, 5, step=2))
-axes[0].set_yticks(np.arange(0, 1.1, step=0.2))
+axes[0].set_yticks(np.arange(0, .5, step=0.1))
 axes[0].legend(loc='upper left', fontsize='x-small')
 
 #w(p) plot
-axes[1].axvline(x=0.5, LineStyle='--')
-axes[1].plot(xx, xx, 'r', lineWidth='2', label=r'$F_p$')
-axes[1].plot(DO, DM, 'b', lineWidth='2', label=r'$F_w$')
+#axes[1].axvline(x=0.5, LineStyle='--')
+axes[1].set_xlim((0,1.05*stat.norm.pdf(0, l1, s1)))
+axes[1].set_ylim((0,1.05*stat.norm.pdf(0, l1, s1)))
+axes[1].plot(xx, xx, 'r', lineWidth='2', label=r'$p$')
+axes[1].plot(DO, DM, 'b', lineWidth='2', label=r'$w$')
 
 # plot arrows
 #nx = len(x)
@@ -61,15 +65,15 @@ axes[1].plot(DO, DM, 'b', lineWidth='2', label=r'$F_w$')
 #        axes[1].arrow(xs[i], xs[i], 0, stat.norm.cdf(stat.norm.ppf(xs[i], l1, s1), l2, s2) - stat.norm.cdf(stat.norm.ppf(xs[i], l1, s1), l1, s1), head_width=0.018, head_length=0.045, length_includes_head=True, fc='k', ec='k', zorder=10)
 
 for i in xs:
-    axes[1].arrow(stat.norm.cdf(i, l1, s1),stat.norm.cdf(i, l1, s1), 0, stat.norm.cdf(i, l2, s2) - stat.norm.cdf(i, l1, s1), head_width=0.018, head_length=0.045, length_includes_head=True, fc='k', ec='k', zorder=10)
+    axes[1].arrow(stat.norm.pdf(i, l1, s1),stat.norm.pdf(i, l1, s1), 0, stat.norm.pdf(i, l2, s2) - stat.norm.pdf(i, l1, s1), head_width=0.008, head_length=0.02, length_includes_head=True, fc='k', ec='k', zorder=10)
 
-axes[1].set_xlabel(r'CDF $F_p$')
-axes[1].set_ylabel(r'CDFs')
-axes[1].set_xticks(np.arange(0, 1.1, step=0.2))
-axes[1].set_yticks(np.arange(0, 1.1, step=0.2))
+axes[1].set_xlabel(r'PDF $p$')
+axes[1].set_ylabel(r'PDFs')
+axes[1].set_xticks(np.arange(0, .5, step=0.1))
+axes[1].set_yticks(np.arange(0, .5, step=0.1))
 axes[1].legend(loc='upper left', fontsize='x-small')
 
-plt.savefig("./../GaussianFvsXtoWvsP.pdf", bbox_inches='tight')
+plt.savefig("./../density_map.pdf", bbox_inches='tight')
 plt.show()
 plt.clf()
 
