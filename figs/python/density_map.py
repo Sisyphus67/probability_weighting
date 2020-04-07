a20ad23c53ee80b9c711f3933b608df36dc1d5a9
nonergodicMark <m.kirstein@lml.org.uk>
2020-04-07 15:57:58 +0200

    Corrected minor typos

--- figs/python/density_map.py
+++ figs/python/density_map.py
@@ -44,7 +44,7 @@ for i in xs:
 #        axes[0].arrow(xs[i], stat.norm.cdf(xs[i], l1, s1), 0, stat.norm.cdf(xs[i], l2, s2) - stat.norm.cdf(xs[i], l1, s1), head_width=0.15, head_length=0.045, length_includes_head=True, fc='k', ec='k', zorder=10)
 
 axes[0].set_xlabel(r'x')
-axes[0].set_ylabel(r'CDFs')
+axes[0].set_ylabel(r'PDFs')
 axes[0].set_xticks(np.arange(-4, 5, step=2))
 axes[0].set_yticks(np.arange(0, .5, step=0.1))
 axes[0].legend(loc='upper left', fontsize='x-small')
